ed9072ad669163a809cc55ba2d948ea2051ba797
liubeijing <liubeijing@meituan.com>
2024-03-21 01:02:49 +0800

    feat: 🎸 test

--- test.ts
+++ test.ts
@@ -19,3 +19,6 @@ function greet(person: Person) {
 console.log(greet(person));
 console.log(greet(person));
 console.log(greet(person));
+console.log(greet(person));
+console.log(greet(person));
+console.log(greet(person));
